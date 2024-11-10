a90b7c738ee29438d924d8dc3827e1f3a1129a98
Johannes May <johannes@may-se.de>
2024-11-10 20:37:42 +0100

    Remove remote storage adapter for influx v1

--- flake.nix
+++ flake.nix
@@ -13,11 +13,6 @@
     formatter.x86_64-linux = pkgs.alejandra;
     nixosModules = {
       prometheus-nixos-system-info = import ./modules/prometheus-nixos-system-info.nix;
-      prometheus-remote-storage = import ./modules/prometheus-remote-storage.nix;
-    };
-    packages.x86_64-linux.prometheus_remote_storage_adapter = import ./pkgs/prometheus_remote_storage_adapter.nix {inherit pkgs;};
-    overlays.default = final: prev: {
-      prometheus_remote_storage_adapter = self.packages.${prev.system}.prometheus_remote_storage_adapter;
     };
   };
 }
